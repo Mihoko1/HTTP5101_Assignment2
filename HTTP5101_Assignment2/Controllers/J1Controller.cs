3a5b590cd379ba1a9db62da4e2c01c53762a2dc4
Mihoko1 <mihoko.s0408@gmail.com>
2020-09-30 23:24:37 -0400

    Add project files.

---
+++ HTTP5101_Assignment2/Controllers/J1Controller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HTTP5101_Assignment2.Controllers
+{
+    public class J1Controller : ApiController
+    {
+        [Route("api/J1/Menu/{burger}/{drink}/{side}/{dessert}")]
+        public string GetMenu(int burger, int drink, int side, int dessert)
+        {
+            int[] burgerCal = { 461, 431, 420, 0 };
+            int[] drinkCal = { 130, 160, 118, 0 };
+            int[] sideCal = { 100, 57, 70, 0 };
+            int[] dessertCal = { 167, 266, 75, 0 };
+
+            int burgerIndex = burger - 1;
+            int drinkIndex = drink - 1;
+            int sideIndex = side - 1;
+            int desserIndex = dessert - 1;
+
+            int totalCal = burgerCal[burgerIndex] + drinkCal[drinkIndex] + sideCal[sideIndex] + dessertCal[desserIndex];
+            return "Your total calorie count is " + totalCal;
+        }
+    }
+}
---
+++ HTTP5101_Assignment2/Controllers/J2Controller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HTTP5101_Assignment2.Controllers
+{
+    public class J2Controller : ApiController
+    {
+        [Route("api/J2/DiceGame/{m}/{n}")]
+        public string GetDiceGame(int m, int n)
+        {
+
+            int sum = 0;
+            int total = 0;
+            string message;
+
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    sum = i + j;
+
+                    if (sum == 10)
+                    {
+                        total++;
+                    }
+                }
+            }
+
+            if (total == 1)
+            {
+               message = "There is " + total + " way to get the sum 10";
+            }
+            else
+            {
+                message = "There are " + total + " ways to get the sum 10";
+
+            }
+            
+            return message; 
+        }
+    }
+}
